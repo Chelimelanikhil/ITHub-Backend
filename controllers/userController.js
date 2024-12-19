7e1fe9d83d21ebe55f7a9a60821d040ed4532186
Chelimela Nikhil <chelimelanani123@gmail.com>
2024-12-19 11:08:52 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,23 @@
 const Company = require('../models/User');
 
 
-
 const getuserdetails = async (req, res) => {
   const id = req.user.id;
 
   try {
-    // Query the company using the `id` parameter
-    const user = await Company.findOne({ _id: id });
+    // Query the user using the `id` parameter, excluding the `password` field
+    const user = await Company.findOne({ _id: id }).select('-password');
 
     if (!user) {
-      return res.status(404).json({ message: 'user not found' });
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json(user.name); // Return the company data
-    
+    res.status(200).json(user); // Return the user data
   } catch (error) {
-    console.error('Error fetching company details:', error); // Log the error for debugging
+    console.error('Error fetching user details:', error); // Log the error for debugging
     res.status(500).json({ message: 'Server error' }); // Return a generic server error response
   }
 };
+
 
 module.exports = { getuserdetails };
